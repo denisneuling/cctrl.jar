37450d824f4dc5ecbcc81c61e48f1ec876ca2de8
Denis Neuling <denisneuling@gmail.com>
2012-11-27 20:30:19 +0100

    Extend and fix issue #3 https://github.com/denisneuling/cctrl.jar/issues/3

--- cctrl-api/src/main/java/com/cloudcontrolled/api/model/AbstractModel.java
+++ cctrl-api/src/main/java/com/cloudcontrolled/api/model/AbstractModel.java
@@ -31,28 +31,28 @@ public abstract class AbstractModel {
 		return uuid;
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		AbstractModel other = (AbstractModel) obj;
-//		if (uuid == null) {
-//			if (other.uuid != null)
-//				return false;
-//		} else if (!uuid.equals(other.uuid))
-//			return false;
-//		return true;
-//	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractModel other = (AbstractModel) obj;
+		if (uuid == null) {
+			if (other.uuid != null)
+				return false;
+		} else if (!uuid.equals(other.uuid))
+			return false;
+		return true;
+	}
 }
